7e46415b67c624ce312f613f8d7dbc0e71a10a44
MarcGenevier <55924907+MarcGenevier@users.noreply.github.com>
2023-03-26 00:16:28 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -16,7 +16,7 @@ def scan_ports(domain_or_ip):
         print(f"Impossible de résoudre le nom de domaine {domain_or_ip}")
         return []
 
-    return nm.scan(ip_add_entered, arguments="-A")
+    return nm.scan(ip_add_entered, arguments="-A -p1-65535 -O -sV")
 
 #  --------------------------------------------------------------------------
 
